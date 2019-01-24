343d91c930614a15bd21083f1071b9e71bc52c4d
Robert.Schreib <robert.schreib@e-mundo.de>
2019-01-24 16:47:54 +0100

    Docker Image mit ubuntu und Multistage Build

--- hooks/build.sh
+++ hooks/build.sh
@@ -24,7 +24,7 @@ echo "Benutze Base Image ${BASE_IMAGE}"
 
 docker build . \
     -t "$TAG" \
-    --build-arg BASE_IMAGE=$BASE_IMAGE \
+    --build-arg JDK_VERSION=$BASE_IMAGE \
     --build-arg GRADLE_VERSION=$FULL_GRADLE_VERSION
 
 IMAGE_ID=$(docker images $TAG --format "{{.ID}}")
