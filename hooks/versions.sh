2cd53e5786311ad7c1cb70e92a5194a4150880d3
Lukas Lisowski <lukas@lisowski.info>
2019-02-07 14:59:19 +0100

    Revert "Image auf ubuntu basis bauen"

--- hooks/versions.sh
+++ hooks/versions.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-export IMAGE='emundo/docker-compose-openjdk-node-gradle'
+
 export VARS='
-BASE_IMAGE=11 GRADLE_VERSION=5.1 IMAGE_TAG="openjdk-11-gradle-5.1"
-BASE_IMAGE=11 GRADLE_VERSION=5.2 IMAGE_TAG="openjdk-11-gradle-5.2"
-BASE_IMAGE=8 GRADLE_VERSION=5.1 IMAGE_TAG="openjdk-8-gradle-5.1"
-BASE_IMAGE=8 GRADLE_VERSION=5.2 IMAGE_TAG="openjdk-8-gradle-5.2"
-BASE_IMAGE=8 GRADLE_VERSION=4.10 IMAGE_TAG="openjdk-8-gradle-4.10" EXTRA_TAGS="openjdk-8;latest"
-BASE_IMAGE=11 GRADLE_VERSION=4.10 IMAGE_TAG="openjdk-11-gradle-4.10" EXTRA_TAGS="openjdk-11"
-BASE_IMAGE=8 GRADLE_VERSION=4.5.1 IMAGE_TAG="gradle-4.5.1"
-BASE_IMAGE=8 GRADLE_VERSION=4.8.1 IMAGE_TAG="gradle-4.8.1" 
-'
+BASE_IMAGE=openjdk:11-jdk GRADLE_VERSION=5.1 IMAGE_TAG="openjdk-11-gradle-5.1"
+BASE_IMAGE=openjdk:8-jdk GRADLE_VERSION=5.1 IMAGE_TAG="openjdk-8-gradle-5.1"
+BASE_IMAGE=openjdk:8-jdk GRADLE_VERSION=4.10 IMAGE_TAG="openjdk-8-gradle-4.10" EXTRA_TAGS="openjdk-8;latest"
+BASE_IMAGE=openjdk:11-jdk GRADLE_VERSION=4.10 IMAGE_TAG="openjdk-11-gradle-4.10" EXTRA_TAGS="openjdk-11"
+BASE_IMAGE=openjdk:8-jdk GRADLE_VERSION=4.5.1 IMAGE_TAG="gradle-4.5.1"
+BASE_IMAGE=openjdk:8-jdk GRADLE_VERSION=4.8.1 IMAGE_TAG="gradle-4.8.1" '
