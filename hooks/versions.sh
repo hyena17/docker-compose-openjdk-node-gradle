206666f2a4ca7aa024f4c8ee597b3b69e6728efb
Lukas Lisowski <lukas@lisowski.info>
2019-02-11 23:36:28 +0100

    add Gradle 5.2 to build

--- hooks/versions.sh
+++ hooks/versions.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 export VARS='
+BASE_IMAGE=openjdk:11-jdk GRADLE_VERSION=5.2 IMAGE_TAG="openjdk-11-gradle-5.2"
 BASE_IMAGE=openjdk:11-jdk GRADLE_VERSION=5.1 IMAGE_TAG="openjdk-11-gradle-5.1"
+BASE_IMAGE=openjdk:8-jdk GRADLE_VERSION=5.2 IMAGE_TAG="openjdk-8-gradle-5.2"
 BASE_IMAGE=openjdk:8-jdk GRADLE_VERSION=5.1 IMAGE_TAG="openjdk-8-gradle-5.1"
 BASE_IMAGE=openjdk:8-jdk GRADLE_VERSION=4.10 IMAGE_TAG="openjdk-8-gradle-4.10" EXTRA_TAGS="openjdk-8;latest"
 BASE_IMAGE=openjdk:11-jdk GRADLE_VERSION=4.10 IMAGE_TAG="openjdk-11-gradle-4.10" EXTRA_TAGS="openjdk-11"
